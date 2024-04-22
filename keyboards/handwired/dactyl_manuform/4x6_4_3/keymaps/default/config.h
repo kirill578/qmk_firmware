c7fac90f4e0395378c38bf96d1b6241964fd82ad
Kirill Kulakov <kirill578@gmail.com>
2024-04-22 17:29:44 -0500

    some fixes

--- keyboards/handwired/dactyl_manuform/4x6_4_3/keymaps/default/config.h
+++ keyboards/handwired/dactyl_manuform/4x6_4_3/keymaps/default/config.h
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MK_C_OFFSET_2 1024
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 200
+#define TAPPING_TERM 320
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
--- keyboards/handwired/dactyl_manuform/4x6_4_3/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6_4_3/keymaps/default/keymap.c
@@ -260,11 +260,11 @@ combo_t key_combos[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
 QK_GESC,    KC_1,           KC_2,           KC_3,               KC_4,           KC_5,      /**/       KC_6,   KC_7,           KC_8,                      KC_9,                       KC_0,            LT(0, KC_EQL),
-KC_TAB,     KC_Q,           KC_W,           KC_F,               KC_P,           KC_B,      /**/       KC_J,   KC_L,           KC_U,                      KC_Y,                       KC_SCLN,         LT(0, KC_MINS),
-KC_BSPC,    HOME_A,         HOME_R,         HOME_S,             HOME_T,         HOME_G,    /**/       HOME_M, HOME_N,         HOME_E,                    HOME_I,                     HOME_O,          LT(0, KC_QUOT),
+KC_TAB,     KC_Q,           KC_W,           KC_F,               KC_P,           KC_B,      /**/       KC_J,   KC_L,           KC_U,                      KC_Y,                       KC_QUOT,         LT(0, KC_MINS),
+KC_BSPC,    HOME_A,         HOME_R,         HOME_S,             HOME_T,         HOME_G,    /**/       HOME_M, HOME_N,         HOME_E,                    HOME_I,                     HOME_O,          LT(0, KC_SCLN),
 PRINT,      LT(0,KC_Z),     LT(0,KC_X),     LT(0,KC_C),         LT(_MOUSE,KC_D),LT(0,KC_V),/**/       KC_K,   KC_H,           KC_COMM,                   KC_DOT,                     KC_SLSH,         LT(0, KC_BSLS),
 KC_NO,      KC_NO,          KC_LPRN,        KC_RPRN,                                       /**/                               LT(0, KC_LBRC),            LT(0, KC_RBRC),             KC_NO,           KC_NO,
-                                                     KC_TAB,  LT(_ARROW,KC_SPC),    KC_NO, /**/     KC_NO,    LT(_SYMBOLS,KC_ENT),     KC_NO
+                                                    KC_TAB,  LT(_ARROW,KC_SPC),    KC_TAB, /**/     KC_NO,    LT(_SYMBOLS,KC_ENT),     KC_NO
     ),
 
 
@@ -280,11 +280,11 @@ KC_NO,      KC_NO,          KC_LPRN,        KC_RPRN,                            
 //    ),
 
     [_SYMBOLS] = LAYOUT(
-KC_TRNS,    KC_F1,          KC_F2,          KC_F3,              KC_F4,          KC_F5,    /**/       KC_F6,   KC_F7,          KC_F8,                    KC_F9,                     KC_F10,          KC_DEL,
-KC_NO,      KC_1,           KC_2,           KC_3,               KC_4,           KC_5,     /**/       KC_6,    KC_7,           KC_8,                     KC_9,                       KC_0,            KC_PLUS,
-KC_TRNS,    KC_EXLM,        KC_AT,          KC_HASH,            KC_DLR,         KC_PERC,  /**/       KC_CIRC, KC_AMPR,        KC_ASTR,                  S(KC_BSLS),                 KC_PLUS,         KC_PIPE,
-KC_TRNS,    KC_GRV,         KC_QUOT,        S(KC_QUOT),         S(KC_SLSH),     KC_GRV,   /**/       KC_TRNS, KC_PLUS,        KC_MINS,                  KC_EQL,                     KC_UNDS,         KC_NO,
-KC_NO,      KC_NO,          KC_LCBR,        KC_RCBR,                                      /**/                                KC_LPRN,                  KC_RPRN,                    KC_NO,           KC_NO,
+KC_TRNS,    KC_F1,          KC_F2,          KC_F3,              KC_F4,          KC_F5,    /**/       KC_F6,   KC_F7,          KC_F8,                    KC_F9,                      KC_F10,          KC_DEL,
+KC_TRNS,    KC_1,           KC_UNDS,        KC_LBRC,            KC_RBRC,        KC_CIRC,     /**/    KC_6,    S(KC_COMM),        S(KC_DOT),                  KC_9,                       KC_0,            KC_PLUS,
+KC_TRNS,    KC_EXLM,        KC_AT,          KC_LCBR,            KC_RCBR,        KC_ASTR,  /**/       KC_CIRC, KC_LPRN,                  KC_RPRN,                    KC_PLUS,         KC_PIPE,    KC_AMPR,
+KC_TRNS,    KC_GRV,         KC_QUOT,        KC_PIPE,            KC_GRV,         KC_GRV,   /**/       KC_TRNS, KC_PLUS,        KC_MINS,                  KC_EQL,                     KC_UNDS,         KC_NO,
+KC_TRNS,    KC_NO,          KC_NO,          KC_NO,                                      /**/                                KC_LPRN,                  KC_RPRN,                    KC_NO,           KC_NO,
 
                                                     KC_TRNS,    KC_TRNS,        KC_TRNS,  /**/  KC_TRNS,  KC_TRNS,   KC_TRNS
     ),
@@ -301,10 +301,10 @@ KC_TRNS,    KC_TRNS,        KC_TRNS,        KC_TRNS,                            
 
     [_ARROW] = LAYOUT(
 QK_BOOT,    DEPLOYTEST,     INVALIATE,      KC_NO,              KC_NO,          KC_NO,    /**/       KC_NO,  KC_NO,          KC_PSLS,                   KC_PAST,                   KC_PMNS,         KC_NO,
-KC_NO,      KC_NO,          KC_NO,          KC_UP,              KC_NO,          KC_NO,    /**/       KC_NO,  KC_P7,          KC_P8,                     KC_P9,                     KC_PPLS,         KC_NO,
-KC_NO,      KC_HOME,        KC_LEFT,        KC_DOWN,            KC_RGHT,        KC_END,   /**/       KC_NO,  RSFT_T(KC_P4),  RGUI_T(KC_P5),             LALT_T(KC_P6),             RCTL_T(KC_PCMM), KC_NO,
-KC_NO,      KC_NO,          KC_MS_BTN4,     KC_NO,              KC_MS_BTN5,     KC_NO,    /**/       KC_P0,  KC_P1,          KC_P2,                     KC_P3,           KC_PEQL,KC_NO,
-KC_NO,      KC_NO,          KC_NO,          KC_NO,                                        /**/                               KC_PENT,                   KC_PDOT,                   KC_NO,           KC_NO,
+KC_TRNS,      KC_NO,          KC_NO,          KC_UP,              KC_NO,          KC_NO,    /**/       KC_NO,  KC_P7,          KC_P8,                     KC_P9,                     KC_PPLS,         KC_NO,
+KC_TRNS,      KC_HOME,        KC_LEFT,        KC_DOWN,            KC_RGHT,        KC_END,   /**/       KC_NO,  RSFT_T(KC_P4),  RGUI_T(KC_P5),             LALT_T(KC_P6),             RCTL_T(KC_PCMM), KC_NO,
+KC_TRNS,      KC_NO,          KC_MS_BTN4,     KC_NO,              KC_MS_BTN5,     KC_NO,    /**/       KC_P0,  KC_P1,          KC_P2,                     KC_P3,           KC_PEQL,KC_NO,
+KC_TRNS,      KC_NO,          KC_NO,          KC_NO,                                        /**/                               KC_PENT,                   KC_PDOT,                   KC_NO,           KC_NO,
 
                                                     KC_TRNS,    KC_LCTL,        KC_DEL,  /**/        KC_BSPC,    KC_NO,      KC_NO
     )
